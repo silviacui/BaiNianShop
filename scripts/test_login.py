7401fece6a4395ac1223b67d58eb9b1a11733b64
silviacui <silviacui317@yeah.net>
2019-07-14 17:53:21 +0800

    补上一步添加的内容

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from base import init_driver
 from page import Page
+import pytest
+from base import data_analyze
 
 
 class TestLogin:
@@ -9,18 +11,24 @@ class TestLogin:
         self.driver = init_driver()
         self.page = Page(self.driver)
 
-    def test_login001(self):
+    @pytest.mark.parametrize("args",data_analyze("test_login"))
+    def test_login(self,args):
+
+        username = args["username"]
+        password = args["password"]
+        expect = args["expect"]
+
         # 点击主页的我图标
         self.page.home.click_mine_icon()
         # 点击我的页面的”已有账号，去登录“
         self.page.mine.click_login_button()
         # 登录页面输入账号和密码，并点击登录按钮
-        self.page.login.input_username("135 54958766")
-        self.page.login.input_password("123000")
+        self.page.login.input_username(username)
+        self.page.login.input_password(password)
         self.page.login.click_login()
         # 判断toast
         # if "此用户不存在" == self.page.login.find_toast("此用户不存在"):
         #     assert True
         # else:
         #     assert False
-        assert self.page.login.if_toast_exist("此用户不存在")
+        assert self.page.login.if_toast_exist(expect)
