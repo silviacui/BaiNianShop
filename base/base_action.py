a9f4836ba17262ccba89a040e0ca3333fcfa6b50
silviacui <silviacui317@yeah.net>
2019-08-16 16:09:59 +0800

    在base中增加，滑动一次，并且滑动一次查找元素的方法

--- base/base_action.py
+++ base/base_action.py
@@ -126,3 +126,64 @@ class BaseAction:
         except Exception:
             return False
 
+    def scroll_one_time(self, dir="up"):
+
+        # 如果不等于滑动页面
+        window_width = self.driver.get_window_size()["width"]
+        window_height = self.driver.get_window_size()["height"]
+        # 取页面尺寸的一半的值
+        centre_x = window_width * 0.5
+        centre_y = window_height * 0.5
+
+        start_x, start_y, end_x, end_y = 0, 0, 0, 0
+
+        if dir == "up" or dir == "down":
+            start_x = centre_x
+            start_y = window_height * 0.75
+            end_x = centre_x
+            end_y = window_height * 0.25
+
+        elif dir == "left" or dir == "right":
+            start_x = window_width * 0.75
+            start_y = centre_y
+            end_x = window_width * 0.25
+            end_y = centre_y
+
+        # 判断dir的参数值
+        if dir == "up" or dir == "left":
+            self.driver.swipe(start_x, start_y, end_x, end_y, 3000)
+        elif dir == "down" or dir == "left":
+            self.driver.swipe(end_x, end_y, start_x, start_y, 3000)
+        else:
+            raise Exception("dir的参数只能是up/down/left/right")
+
+    def if_scroll_until_feature_found(self, feature, feature_text, dir):
+        """
+        滑动当前页面，直到目标feature已经找到
+        :param feature: 定位元素的feature
+        :param feature_text: 定位元素的text
+        :param dir: 滑动页面的方向，只能输入参数up/down/left/right
+        :return:
+        """
+        old = ""
+        # 无限循环
+        while True:
+            new = ""
+            # 用find_elements定位到settings中所有的列表元素
+            eles = self.find_elements(feature)
+            # for循环取每个元素的text，判断是否等于“关于手机”，如果等于停止执行
+            for i in eles:
+                text = i.text
+                # 把每一轮定位到的所有元素的text拼接到new里面
+                new += text
+
+                if text == feature_text:
+                    return True
+
+            if  old == new:
+                return False
+            else:
+                old = new
+
+            # 如果new不等于old，则继续滑动
+            self.scroll_one_time(dir)
--- page/settings_page.py
+++ page/settings_page.py
@@ -7,6 +7,16 @@ class SettingsPage(BaseAction):
 
     # 地址管理按钮
     manage_address_button = By.XPATH, "text,地址管理"
+    # 关于百年里奥按钮
+    about_bainian_button = By.XPATH, "text,关于百年里奥"
+    feature_list = By.XPATH, "package,com.yunmall.lc"
 
     def click_manage_address_button(self):
         self.click(self.manage_address_button)
+
+    # def if_find_about_bainian_button(self):
+    #     return self.if_scroll_until_feature_found(self.feature_list, "关于百年里奥","up")
+    #
+    # def click_about_bainian_button(self):
+    #     self.click(self.about_bainian_button)
+
